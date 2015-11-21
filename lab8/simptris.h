a936ddc930de08723bed9abf0ceb8bf80dc8ef8f
rlyon14 <rlyon14@gmail.com>
2015-11-20 17:49:55 -0700

    improved score on lab8

--- lab8/simptris.h
+++
@@ -1,7 +0,0 @@
-// Simptris Command Function Header File
-
-void SlidePiece(int ID, int direction);
-void RotatePiece(int ID, int direction);
-void SeedSimptris(long seed);
-void StartSimptris(void);
-
